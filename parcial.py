18ab9af855838f904a00794dee9e5c7da5569bb5
barbozamatias5 <barbozamatiasg.05@gmail.com>
2025-06-29 22:13:23 -0300

    funcionalidad de texto

--- parcial.py
+++ parcial.py
@@ -1,6 +1,8 @@
 import pygame
+import sys
 from personaje import *
 from ahorcado import *
+from textos_juego import *
 
 # Inicializar Pygame
 pygame.init()
@@ -20,32 +22,46 @@ BLANCO = (128, 128, 128)
 # Cargar imagen del personaje (debe estar en el mismo directorio o indicar ruta completa)
 
 error = 0
+estado = True
+estado_jugar = False
 # Cargar personaje (usá tu imagen, por ejemplo: "personaje.png")
 personaje = crear_personaje(x = 500, y= 10)
+boton_jugar = texto(x= 300,y= 300, ruta ="assets/boton_empezar.png")
 
 # Bucle principal
 ejecutando = True
 while ejecutando:
+
     for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            ejecutando = False
+            if evento.type == pygame.QUIT:
+                ejecutando = False
+                pygame.quit()
+                sys.exit()
+
+    if estado_jugar:
         if evento.type == pygame.KEYDOWN:
             error += 1
             if evento.key == pygame.K_ESCAPE:
                 ejecutando = False
 
-    # Movimiento con teclado
-    teclas = pygame.key.get_pressed()
-    movimiento(personaje, teclas, ancho_pantalla = ANCHO)
+        # Movimiento con teclado
+        teclas = pygame.key.get_pressed()
+        movimiento(personaje, teclas, ancho_pantalla = ANCHO)
 
-    if error == 7:
-        error = 0
+        if error == 7:
+            estado = False
 
-    # Dibujar todo
+        # Dibujar todo
+        
+        dibujar_personaje(pantalla, personaje)
+        dibujar_ahorcado(
+                pantalla, crear_ahorcado(fallo = error, x = 400, y = 154))
+    
+    else:
+        dibujo_textos(boton_jugar, pantalla)
+
     pantalla.fill(BLANCO)
-    dibujar_personaje(pantalla, personaje)
-    dibujar_ahorcado(
-        pantalla, crear_ahorcado(fallo = error, x = 400, y = 154))
+    pygame.display.flip()
     pygame.display.update()
     RELOJ.tick(30)
 
--- textos_juego.py
+++ textos_juego.py
@@ -1,40 +1,41 @@
 import pygame
 
-def textos(x, y, imagen, escalado = 1):
+def texto(x, y, ruta, escalado = 1):
     '''
     
     
     
     '''
-    ancho = imagen.get_width()
-    alto = imagen.get_height()
-    imagen = pygame.transform.scale(imagen, (int(ancho * escalado), int(alto * escalado)))
-    rectangulo_imagen = imagen.get_rect()
-    rectangulo_imagen.topleft = (x, y)
+    image = pygame.image.load(ruta)
+    ancho = image.get_width()
+    alto = image.get_height()
+    image_escalada = pygame.transform.scale(image, (int(ancho * escalado), int(alto * escalado)))
+    rectangulo = image_escalada.get_rect()
+    rectangulo.topleft = (x, y)
+    clickeado = False
 
     return {
-        "imagen": imagen,
-        "rect": rectangulo_imagen,
-        "clickeado": False
+        "imagen": image_escalada.convert_alpha(),
+        "rect": rectangulo,
+        "clickeado": clickeado
     }
 
-def dibujo_textos(texto: function, pantalla):
+def dibujo_textos(texto, pantalla):
     '''
     
     
     
     '''
     accion = False
-
     mouse = pygame.mouse.get_pos()
 
-    if texto.rect.collidepoint(mouse):
+    if texto["rect"].collidepoint(mouse):
         if pygame.mouse.get_pressed()[0] == 1 and texto["clickeado"] == False:
             accion = True
             texto["clickeado"] = True
     if pygame.mouse.get_pressed()[0] == 0:
         texto["clickeado"] = False
     
-    pantalla.blit(texto, (texto["rect".x], texto["rect"].y))
+    pantalla.blit(texto["imagen"], (texto["rect"].x, texto["rect"].y))
 
     return accion
